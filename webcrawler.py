ce741df1113f5e2b3dfa3d12d4e609307ca44780
lexylena <lexylena@gmail.com>
2017-11-10 13:10:54 -0500

    getting 403

--- webcrawler.py
+++ webcrawler.py
@@ -26,11 +26,11 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.settimeout(10.30)
 print("connecting")
 try:
-    sock.connect(('129.10.113.143',80))
+    sock.connect(('fring.ccs.neu.edu',80))
 except Exception as e: 
     print("something's wrong %s" % e)
 print("sending")
-sendthis = 'GET /accounts/login/?next=/fakebook/ HTTP/1.1\nHost:129.10.113.143:80\n\n'
+sendthis = 'GET /accounts/login/?next=/fakebook/ HTTP/1.1\nHost: fring.ccs.neu.edu\n\n'
 sock.sendall(sendthis.encode('utf-8'))
 print("sent")
 print("waiting for response")
@@ -39,13 +39,16 @@ data = data.decode('utf-8')
 #print(data)
 parser.feed(data)
 print("send login")
-body = 'username=' + username + '&password=' + password
-sendlogin = 'POST /accounts/login/ HTTP/1.1\nHost: 129.10.113.143:80\nConnection: keep-alive\nCookie: csrftoken=' + token +'\nContent-Type: application/x-www-form-urlencoded\nContent-Length:'+ str(len(body)) + '\n\n'+ body +'\n\n'
+body = 'id_username=' + username + '&id_password=' + password
+sendlogin = ('POST /accounts/login/ HTTP/1.1\nHost: fring.ccs.neu.edu\nConnection: keep-alive\n' +
+             'Cookie: csrftoken=' + token + '\nAccept:text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8'+
+             '\nAccept-Encoding:gzip, deflate' +'\nOrigin: http://fring.ccs.neu.edu\nReferer:http://fring.ccs.neu.edu/accounts/login/?next=/fakebook/'+
+             '\nContent-Type: application/x-www-form-urlencoded\nContent-Length:'+ str(len(body)) + '\n\n'+ body +'\n\n')
 print(sendlogin)
 sock.sendall(sendlogin.encode('utf-8'))
 print("sent")
 data2 = (sock.recv(1700000))
-print("data2", data2.decode('utf-8'))
+print("data2", data2)
 sock.shutdown(1)
 sock.close()
 print("token",token)
